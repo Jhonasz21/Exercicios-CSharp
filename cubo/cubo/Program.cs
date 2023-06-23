51e4f388cc17fec57cf20bf7aa48dd957618fba8
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-23 16:18:27 -0300

    Add files via upload

---
+++ cubo/cubo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cubo
+{
+    //1. Escreva um programa em C# para exibir o cubo do número inteiro. O usuário deve
+    //informar a quantidade de inteiros que deve ser calculado.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n Informe a quantidades de inteiros: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                double resultado = Math.Pow(i, 3);
+                Console.WriteLine("O cubo do número {0} é: {1}", i, resultado);
+            }
+            Console.ReadKey();
+        }
+    }
+}
