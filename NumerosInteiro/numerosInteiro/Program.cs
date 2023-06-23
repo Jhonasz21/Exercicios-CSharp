2bd4a5cdb84ac9ccf91a87785179c2bc849d48c8
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-23 20:59:48 -0300

    Add files via upload

---
+++ NumerosInteiro/numerosInteiro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerosInteiro
+{
+    // Construa um algoritmo que faça a soma dos N primeiros números inteiros. Determine
+    // que o usuário forneça a quantidade N
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, soma;
+
+            Console.WriteLine("Digite valor de N: ");
+            N = Int32.Parse(Console.ReadLine());
+
+            soma = (N * (N + 1)) / 2;
+            Console.WriteLine("Soma dos {0} primeiros números é: {1}", N, soma);
+            Console.ReadKey();
+        }
+    }
+}
