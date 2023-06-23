981666fd1d82b63f714e40a144e14593a0c8488f
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-23 16:21:56 -0300

    Add files via upload

---
+++ LoginSenha/LoginSenha/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System .Globalization;
+namespace exercicio1 {
+    class Program { 
+        static void Main(string[] args) {
+
+          int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido");
+
+    }
+  }
+}
