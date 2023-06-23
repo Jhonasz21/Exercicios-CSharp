6df9c23e9ee42e916100cc5cb9a3e363616a47b0
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-23 20:59:01 -0300

    Add files via upload

---
+++ ValorInteiroImpar/For/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    internal class Program
+    {
+        /*
+         * Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, 
+         * inclusive o X, se for o caso.
+         * 
+         */
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+                
+            for (int i = 1; i < x; i++) { 
+            if (i % 2 != 0) { 
+                Console.WriteLine(i);
+                }
+            }
+            
+            
+    }
+  }
+}
