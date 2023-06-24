4ae1054c7f4f448e3b9b9e6b66e2aa5e4dd41c46
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-24 12:04:42 -0300

    Add files via upload

---
+++ nome5Amigos/nome5Amigos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace nome5Amigos
+{
+    internal class Program
+    {
+        /* Criar uma lista com o nome de 5 amigos e depois ordene-a em ordem alfabética.
+         */
+        static void Main(string[] args)
+        {
+            List<string> amigos = new List<string> { "Akita", "Smith", "Jackson", "Bacon", "Daniel" };
+
+            Console.WriteLine("Nomes do amigos na lista:\n ", amigos);
+            foreach(string nomes in amigos)
+            {
+                Console.WriteLine("{0}", nomes);
+            }
+
+            amigos.Sort();
+            Console.WriteLine("Nomes em ordem alfabética:\n ");
+            foreach(string nomes in amigos)
+            {
+                Console.WriteLine("{0}", nomes);
+            }
+            Console.ReadKey();
+        }
+    }
+}
