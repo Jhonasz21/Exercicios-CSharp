d8e3dc3cabed9d204292a0f0e74b38945f7b9953
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-24 12:17:22 -0300

    Add files via upload

---
+++ ListaSeusQuadrados/list/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace list
+{
+    /* Escrever um programa C# para criar uma lista de inteiros e apresente uma nova lista com os seus quadrados.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> Numeros = new List<int>() { 1, 3, 5, 7, 8 };
+
+                Console.WriteLine("Lista de Numeros");
+
+                foreach (int Numero in Numeros)
+
+                {
+                    Console.WriteLine("{0}", Numero);
+
+                }
+           
+            
+                Console.WriteLine("\nNúmeros em uma lista de quadardos");
+
+                foreach (int Numero in Numeros)
+
+                {
+
+                    Console.WriteLine(" {0}", Numero * Numero);
+
+                }
+                
+                Console.ReadKey();
+            }   
+              catch(Exception ex)
+            {
+                Console.WriteLine("Ocorreu erro ao executar o codigo: ");
+            }  
+
+                
+        }
+    }
+}
