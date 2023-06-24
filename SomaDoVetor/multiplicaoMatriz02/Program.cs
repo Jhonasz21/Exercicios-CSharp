12e4f7fee0d9197282241db73dc0c8651f8f7c01
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-23 21:20:34 -0300

    Add files via upload

---
+++ SomaDoVetor/multiplicaoMatriz02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somaDoVetor
+{
+    /* Escreva um programa em C# para encontrar a soma de todos os elementos
+     * de um vetor de tamanho 10 e que mostre esses elementos na tela..
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int soma = 0;
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                soma += vetor[i];
+            }
+
+            Console.WriteLine("A soma do vetor é: " + soma);
+            Console.ReadKey();
+        }
+    }
+}
