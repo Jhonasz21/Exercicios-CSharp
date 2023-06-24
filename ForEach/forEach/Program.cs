747686b15f82f6a68bcf3005e17ee64b56afac8b
Jhonasz21 <53380136+Jhonasz21@users.noreply.github.com>
2023-06-23 21:26:10 -0300

    Add files via upload

---
+++ ForEach/forEach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> List = new List<string>();
+            {
+                List.Add("Maria");
+                List.Add("Alex");
+                List.Add("Bob");
+                List.Add("Anna");
+            {
+            foreach (string s in List)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
